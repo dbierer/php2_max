9f9a91d98fd6e42d05524d5c45baa4b9a75a7798
Max Simon <msimon@leviy.com>
2022-09-18 20:44:33 +0200

    2nd lesson

---
+++ src/App/Models/Customer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Classes;
+
+class Customer extends Person
+{
+    private string $customerStatus;
+
+    public function getCustomerStatus(): string
+    {
+        return $this->customerStatus;
+    }
+
+    public function setCustomerStatus(string $customerStatus): void
+    {
+        $this->customerStatus = $customerStatus;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getFirstName() . ' ' . $this->getLastName();
+    }
+}
