f78d40d08489fd2423f50cfb6803b843d1e1e055
Max Simon <msimon@leviy.com>
2022-09-20 23:53:15 +0200

    3rd lesson

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Exceptions\CreateUserException;
 use App\Models\Customer;
 use App\Models\Employee;
+use App\Models\Person;
+use App\Models\User;
 
 session_start();
 spl_autoload_register(static function ($class) {
@@ -12,11 +15,21 @@ spl_autoload_register(static function ($class) {
 $employee = new Employee('Max', 'Simon');
 $customer = new Customer('James', 'Bond');
 
+$user = new User($customer);
+
 $employee->setEmploymentStatus(true);
 $customer->setCustomerStatus(true);
 
 echo $customer->getCustomerStatus();
 echo "\n" . $employee;
 echo "\n" . $customer . "\n";
+
+try {
+    $user->create('JBond');
+} catch (CreateUserException $e) {
+    echo $e;
+} finally {
+    echo "To resolve this set a password \n";
+}
 
 var_dump($employee);
